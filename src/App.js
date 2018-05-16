76cb657f4d1e6f17c23baafcdabc4d365ff9768c
Matias  Mikkola <matu.mikkola@gmail.com>
2018-05-15 23:00:03 -0700

    Add initial form elements

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Everyone hates form-elements. Here is some reset and default styles for them:</h1>
+        <input name="name" type="text" />
+        <select name="cars">
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="fiat">Fiat</option>
+          <option value="audi">Audi</option>
+        </select>
+        <textarea>Some text...</textarea>
+        <input type="radio" name="gender" value="male" checked /> Male<br />
+        <input type="radio" name="gender" value="female" /> Female<br />
+        <input id="checkBox" type="checkbox" /> checkbox
       </div>
     );
   }
